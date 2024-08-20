7916e00414767a11a132c957366a148d74c2335f
Binyam Yohannes Olango <olangobinyam@gmail.com>
2024-08-20 21:13:19 +0200

    Fill the product details in the template

--- index.js
+++ index.js
@@ -29,6 +29,25 @@ const url = require('url');
 ////////////////////////////////////
 // Web servers
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.name);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NON_ORGANIC%}/g, 'not-organic');
+
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
@@ -37,7 +56,12 @@ const server = http.createServer((req, res) => {
 
     // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW');
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+
+        let output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
     
     // Product page
     } else if (pathName === '/product') {
